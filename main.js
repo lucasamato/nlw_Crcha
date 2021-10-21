fd0a60a338b2cddc87f70370a764d29a1a335064
lucasamato <54944707+lucasamato@users.noreply.github.com>
2021-10-21 20:59:32 -0300

    upload files

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'lucasamato',
+  youtube: 'lucasamato',
+  facebook: 'lucasamato',
+  instagram: 'lucas_amato',
+  twitter: 'lucasamato'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfile() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfile()
